9a6743d797800b0112358045ccc3d6a9e8bb8449
Mitzi Vite <vit20001@byui.edu>
2023-05-23 20:44:37 -0600

    changes navbar

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,2 +1,8 @@
-// const fulldate = new. Int1.DateTimeFormat("en-US", {dateStyle: "full"}). format(new Date());
-// document.querySelector("#fulldate").textContent = fulldate;
+menubutton = document.querySelector("#menu-button");
+
+menubutton.addEventListener("click", () =>
+{
+    document.querySelector("#navbar").classList.toggle('active');
+    document.querySelector("#menu-open").classList.toggle('active');
+    document.querySelector("#menu-close").classList.toggle('active');
+});
