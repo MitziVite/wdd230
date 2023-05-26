823efa9f5ba457a8e0c6aa40abe84b04326c0c59
Mitzi Vite <vit20001@byui.edu>
2023-05-25 22:33:52 -0600

    s

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,8 +1,11 @@
 menubutton = document.querySelector("#menu-button");
 
 menubutton.addEventListener("click", () =>
+
 {
+    
     document.querySelector("#navbar").classList.toggle('active');
     document.querySelector("#menu-open").classList.toggle('active');
     document.querySelector("#menu-close").classList.toggle('active');
+
 });
