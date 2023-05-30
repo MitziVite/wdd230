fb531c8204f9fa019ecf496b3399e0bb3ee10b38
Mitzi Vite <vit20001@byui.edu>
2023-05-30 10:54:20 -0600

    no nav

--- lesson3/script/main.js
+++ lesson3/script/main.js
@@ -1,8 +1,8 @@
-menubutton = document.querySelector("#menu-button");
+// menubutton = document.querySelector("#menu-button");
 
-menubutton.addEventListener("click", () =>
-{
-    document.querySelector("#navbar").classList.toggle('active');
-    document.querySelector("#menu-open").classList.toggle('active');
-    document.querySelector("#menu-close").classList.toggle('active');
-});
+// menubutton.addEventListener("click", () =>
+// {
+//     document.querySelector("#navbar").classList.toggle('active');
+//     document.querySelector("#menu-open").classList.toggle('active');
+//     document.querySelector("#menu-close").classList.toggle('active');
+// });
