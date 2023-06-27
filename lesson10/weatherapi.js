b0359328edd8fc7da6d93971c3f9178171ca7f18
Mitzi Vite <vit20001@byui.edu>
2023-06-27 14:15:45 -0600

    weather

--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -9,10 +9,9 @@ const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&l
 
 function displayResults(weatherData) {
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-    weatherIcon.src = "http://openweathermap.org/img/wn/"
-    + `${weatherData.weather[0].icon}@2x.png`;
-    captionDesc.innerText = weatherData.weather[0].description;
-    return true;
+    weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`);
+    const desc = weatherData.weather[0].description;
+    captionDesc.textContent = desc;
 }
 
 async function apiFetch(url) {
@@ -21,7 +20,7 @@ async function apiFetch(url) {
       if (response.ok) {
         const data = await response.json();
         console.log(data); // this is for testing the call
-        // displayResults(data);
+        displayResults(data);
       } else {
           throw Error(await response.text());
       }
