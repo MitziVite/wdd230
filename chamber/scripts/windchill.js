93c2d00eb3d10e1a6bf48d6f0eac3c858b933737
Mitzi Vite <vit20001@byui.edu>
2023-07-01 09:37:17 -0600

    windspeed

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,22 +1,55 @@
-function windchill(windspeed, temp){
+// select HTML elements in the document
+const currentTem = document.querySelector('#current-temp');
+const windspeedS = document.querySelector('#windspeed');
+const windchill = document.querySelector('#windchill');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
 
-    //Get HTML element to update
-const tempElement = document.querySelector("#temp");
-const windspeedElement  = document.querySelector("#wind-speed");
-const windchillElement = document.querySelector("#wind-chill");
+const lat = 19.43;
+const lon =  99.13;
+const appid = "2c9692c4d272bccd1f50b2dad5746a24";
+const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}&units=imperial`
 
-    //Calculate windchill, if applicable
-    let windchillmsg = "N/A";
-    if (windspeed > 3.0 && temp <= 50){
-        let chillfactor = Math.pow(windspeed, 0.16);
+function calcWindChill(temp, speed) {
+    if (speed > 3.0 && temp <= 50){
+        let chillfactor = Math.pow(speed, 0.16);
         let windchillamt = Math.round(35.74 + (0.6215 * temp) - (35.75 * chillfactor) + (0.4275 * temp * chillfactor));
-        windchillmsg = `${windchillamt}`;
+        return windchillamt
     }
-    //Update HTML element with values
-    tempElement.textContent = temp;
-    windspeedElement.textContent = windspeed;
-    windchillElement.textContent = windchillmsg;
+    return 'N/A'
 }
 
-//call windchill
-windchill(5, 45);
+function displayResults(weatherData) {
+    // api values 
+    // toFixed(0) rounds the temperature to the nearest whole number
+    const temp = weatherData.main.temp.toFixed(0)
+    const windspeed = weatherData.wind.speed.toFixed(0)
+    const desc = weatherData.weather[0].description;
+    // You can use @2x or @4x to make the icon bigger, or omit it for the standard size
+    const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`
+    
+    // html elements
+    currentTem.innerHTML = `<strong>${temp}</strong>`;
+    windspeedS.innerHTML = `<strong>${windspeed}</strong>`
+    windchill.innerHTML = `<strong>${calcWindChill(temp, windspeed)}</strong>`
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc; 
+  }
+
+async function getWeather() {
+    try {
+      const response = await fetch(weatherURL);
+      if (response.ok) {
+        const data = await response.json();
+        // console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+getWeather();
+  
