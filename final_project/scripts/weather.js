8049a3f8e0b099c4aa7fd20ae58a4cb1bcd7f471
Mitzi Vite <vit20001@byui.edu>
2023-07-19 22:41:11 -0600

    sdfsdf

--- final_project/scripts/weather.js
+++ final_project/scripts/weather.js
@@ -1,100 +1,96 @@
-  const lat2 = 19.43;
-  const lon2 = 99.13;
-  const appid = "aec8372b3538aac5be3fb27c66198b48";
-  const weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat2}&lon=${lon2}&appid=${appid}`;
+const lat1 = 19.43;
+const lon1 = 99.13;
+const appid1 = "aec8372b3538aac5be3fb27c66198b48";
+const weatherURL1 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat1}&lon=${lon1}&appid=${appid1}`;
 
-  function kelvinToFahrenheit(temp) {
-    return ((temp - 273.15) * 9 / 5 + 32).toFixed(0);
+function kelvinToFahrenheit(temp) {
+  return ((temp - 273.15) * 9 / 5 + 32).toFixed(0);
+}
+
+function getDayOfWeek(date) {
+  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const dayIndex = date.getDay();
+  return daysOfWeek[dayIndex];
+}
+
+function createWeatherCard(data, isCurrent, dayOfWeek) {
+  const date = new Date(data.dt * 1000); // Convertir la fecha UNIX a una fecha legible
+  const temp = kelvinToFahrenheit(data.main.temp);
+  const desc = data.weather[0].description;
+  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  const hum = data.main.humidity;
+  const tempMin = kelvinToFahrenheit(data.main.temp_min);
+  const tempMax = kelvinToFahrenheit(data.main.temp_max);
+
+  const weatherCard = document.createElement("div");
+  weatherCard.className = "weather-card";
+
+  const dateHeading = document.createElement("h3");
+  dateHeading.textContent = isCurrent ? "today" : dayOfWeek;
+
+  const figure = document.createElement("figure");
+  const weatherIcon = document.createElement("img");
+  weatherIcon.src = iconsrc;
+  weatherIcon.alt = "";
+  const figcaption = document.createElement("figcaption");
+  figcaption.textContent = desc;
+  figure.appendChild(weatherIcon);
+  figure.appendChild(figcaption);
+
+  const tempParagraph = document.createElement("p");
+  tempParagraph.innerHTML = `<strong>${temp}</strong> &deg;F`;
+
+  weatherCard.appendChild(dateHeading);
+  weatherCard.appendChild(figure);
+  weatherCard.appendChild(tempParagraph);
+
+  if (isCurrent) {
+    const humidityParagraph = document.createElement("p");
+    humidityParagraph.textContent = `Humidity: ${hum}%`;
+    weatherCard.appendChild(humidityParagraph);
+  } else {
+    const tempMinMaxParagraph = document.createElement("p");
+    tempMinMaxParagraph.innerHTML = `High: <strong>${tempMax}</strong> &deg;F<br>Low: <strong>${tempMin}</strong> &deg;F`;
+    weatherCard.appendChild(tempMinMaxParagraph);
   }
 
-  function getDayOfWeek(date) {
-    const daysOfWeek = ["domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"];
-    const today = new Date().setHours(0, 0, 0, 0);
+  return weatherCard;
+}
 
-    if (date.setHours(0, 0, 0, 0) === today) {
-      return "today";
+async function getWeather() {
+  try {
+    const response = await fetch(weatherURL1);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data);
     } else {
-      const dayIndex = date.getDay();
-      return daysOfWeek[dayIndex];
+      throw Error(await response.text());
     }
+  } catch (error) {
+    console.log(error);
   }
+}
 
-  function displayResults(weatherData) {
-    const currentWeather = weatherData.list[0];
-    const forecasts = weatherData.list.slice(1, 5); // Obtener los siguientes 4 elementos de la lista de pronósticos
-    const weatherContainer = document.getElementById("weather-container");
+function displayResults(weatherData) {
+  const currentWeather = weatherData.list[0];
+  const forecasts = weatherData.list.slice(1, 4); // Cambiar a 5 para obtener cuatro pronósticos
+  const weatherContainer = document.getElementById("weather-container");
 
-    const currentWeatherCard = createWeatherCard(currentWeather, true);
-    weatherContainer.appendChild(currentWeatherCard);
+  const currentWeatherCard = createWeatherCard(currentWeather, true);
+  weatherContainer.appendChild(currentWeatherCard);
 
-    let nextDate = new Date(currentWeather.dt * 1000);
-    forecasts.forEach(forecast => {
-      const forecastCard = createWeatherCard(forecast, false);
-      weatherContainer.appendChild(forecastCard);
-      nextDate.setDate(nextDate.getDate() + 1); // Incrementar la fecha en un día
-    });
-  }
+  let tomorrow = new Date(); // Obtener la fecha de mañana
+  tomorrow.setDate(tomorrow.getDate() + 1); // Establecer la fecha para mañana
 
-  function createWeatherCard(data, isCurrent) {
-    const date = new Date(data.dt * 1000); // Convertir la fecha UNIX a una fecha legible
-    const dayOfWeek = getDayOfWeek(date); // Obtener el día de la semana
-    const temp = kelvinToFahrenheit(data.main.temp);
-    const desc = data.weather[0].description;
-    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-    const hum = data.main.humidity;
-    const tempMin = kelvinToFahrenheit(data.main.temp_min);
-    const tempMax = kelvinToFahrenheit(data.main.temp_max);
+  forecasts.forEach(forecast => {
+    // Calcular el día de la semana para el pronóstico actual (comenzando desde mañana)
+    const dayOfWeek = getDayOfWeek(tomorrow);
+    const forecastCard = createWeatherCard(forecast, false, dayOfWeek);
+    weatherContainer.appendChild(forecastCard);
 
-    const weatherCard = document.createElement("div");
-    weatherCard.className = "weather-card";
+    // Avanzar al siguiente día para el próximo pronóstico
+    tomorrow.setDate(tomorrow.getDate() + 1);
+  });
+}
 
-    const dateHeading = document.createElement("h3");
-    dateHeading.textContent = dayOfWeek;
-    // dateHeading.appendChild(list.dt)
-     // Mostrar el día de la semana en lugar de la fecha completa
-
-    const figure = document.createElement("figure");
-    const weatherIcon = document.createElement("img");
-    weatherIcon.src = iconsrc;
-    weatherIcon.alt = "";
-    const figcaption = document.createElement("figcaption");
-    figcaption.textContent = desc;
-    figure.appendChild(weatherIcon);
-    figure.appendChild(figcaption);
-
-    const tempParagraph = document.createElement("p");
-    tempParagraph.innerHTML = `<strong>${temp}</strong> &deg;F`;
-
-    weatherCard.appendChild(dateHeading);
-    weatherCard.appendChild(figure);
-    weatherCard.appendChild(tempParagraph);
-
-    if (isCurrent) {
-      const humidityParagraph = document.createElement("p");
-      humidityParagraph.textContent = `Humidity: ${hum}%`;
-      weatherCard.appendChild(humidityParagraph);
-    } else {
-      const tempMinMaxParagraph = document.createElement("p");
-      tempMinMaxParagraph.innerHTML = `High: <strong>${tempMax}</strong> &deg;F<br>Low: <strong>${tempMin}</strong> &deg;F`;
-      weatherCard.appendChild(tempMinMaxParagraph);
-    }
-
-    return weatherCard;
-  }
-
-  async function getWeather() {
-    try {
-      const response = await fetch(weatherURL);
-      if (response.ok) {
-        const data = await response.json();
-        displayResults(data);
-      } else {
-        throw Error(await response.text());
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  getWeather();
-
+getWeather();
