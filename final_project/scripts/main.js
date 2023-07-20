3a56bc46c95cfa6ccd0c31ab3b65960678aac24c
Mitzi Vite <vit20001@byui.edu>
2023-07-20 00:24:09 -0600

    sdf

--- final_project/scripts/main.js
+++ final_project/scripts/main.js
@@ -1,5 +1,13 @@
 
+menubutton = document.querySelector("#menu-button");
 
+menubutton.addEventListener("click", () =>
+{
+    document.querySelector("#navbar").classList.toggle('active');
+    document.querySelector("#menu-close").classList.toggle('active');
+    document.querySelector("#menu-open").classList.toggle('active');
+
+});
   const currentDate = moment();
   const formattedDate = currentDate.format("MM/DD/YYYY  HH:mm:ss");
   document.querySelector("#date2").textContent = formattedDate;
