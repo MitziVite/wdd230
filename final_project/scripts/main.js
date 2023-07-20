939517ed260539e24839a16bdb67c46ed8c8b264
Mitzi Vite <vit20001@byui.edu>
2023-07-19 22:59:34 -0600

    imagessdf

--- final_project/scripts/main.js
+++ final_project/scripts/main.js
@@ -4,3 +4,8 @@
   const formattedDate = currentDate.format("MM/DD/YYYY  HH:mm:ss");
   document.querySelector("#date2").textContent = formattedDate;
 
+  // Get the total number of specialty drinks from localStorage
+const totalDrinks = parseInt(localStorage.getItem("totalDrinks")) || 0;
+
+// Display the total number of drinks in the information card
+document.getElementById("total-drinks-output").textContent = totalDrinks.toString();
